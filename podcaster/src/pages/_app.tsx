0a2e6f5c5495e990ac22436854fc67f722cbc612
croc31 <clauberogerioo@gmail.com>
2022-01-04 15:13:55 -0300

    aula 2

--- podcaster/src/pages/_app.tsx
+++ podcaster/src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.scss'
+import styles from '../styles/app.module.scss'
 import type { AppProps } from 'next/app'
+import { Header } from '../components/Header'
+import { Player } from '../components/Player'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player></Player>
+    </div>
+  );
+
 }
 
 export default MyApp
--- podcaster/src/pages/index.tsx
+++ podcaster/src/pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Header } from '../components/Header'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <h1>Hello World</h1>
+    <div>oi</div>
   )
 }
 
